46b7f0bb94afc8bfd0bbc0492a0d1147867cd97b
christian <rollawaypoint@gmail.com>
2019-12-07 22:29:49 +0800

    Delete service worker avoid https://github.com/gatsbyjs/gatsby/issues/17728

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = ({
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-remove-trailing-slashes`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`
+    `gatsby-plugin-sitemap`
+    // `gatsby-plugin-offline`
   ]
 })
